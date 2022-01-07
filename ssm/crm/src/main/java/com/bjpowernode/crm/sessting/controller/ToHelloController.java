34ad8763c8dd8b999bdbec65bcb4e54114ce4b63
1998msl <1476334926@qq.com>
2022-01-07 14:59:52 +0800

    提交hellogit

---
+++ ssm/crm/src/main/java/com/bjpowernode/crm/sessting/controller/ToHelloController.java
@@ -0,0 +1,10 @@
+package com.bjpowernode.crm.sessting.controller;
+
+/**
+ * 2022/1/7
+ */
+public class ToHelloController {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
